99f4db69991bb6b26d24f572b55de6118946df25
juliagrenke <129645624+juliagrenke@users.noreply.github.com>
2023-04-08 18:32:56 +0500

    Квадрат числа - 1-я задача семинара

---
+++ SeminarEx001_Square/Program.cs
@@ -0,0 +1,15 @@
+﻿//написать программу, которая на вход принимает число, на выход выводит его квадрат
+//Например
+// -3->9
+// -4->16
+// -7->49
+
+int number;
+
+Console.Write ("Введите число: ");
+string input = Console.ReadLine ()!; //! добавляем для того, чтобы при пустом вводе не было ошибки
+
+number = int.Parse (input);
+int Square = number*number;
+Console.Write ("Квадрат числа = ");
+Console.Write (Square);
