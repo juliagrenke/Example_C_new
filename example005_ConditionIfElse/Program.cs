db4881ebddb3c9c383f6a5ed8c32df81bb696825
juliagrenke <129645624+juliagrenke@users.noreply.github.com>
2023-04-08 15:13:08 +0500

    example005_ConditionIfElse project add

---
+++ example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша") //переводит все символы в нижний регистр
+{
+    Console.WriteLine ("Ой-ой, это же Маша!!!");
+}
+else 
+{
+    Console.Write("Привет, ");
+    Console.Write (username);
+}
