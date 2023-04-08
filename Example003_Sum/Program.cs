be1bf97676b40db4df3eee5f92aa7fb611c44919
juliagrenke <129645624+juliagrenke@users.noreply.github.com>
2023-04-08 14:52:46 +0500

    add Random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,5 +1,7 @@
-﻿int numberA = 31;
-int numberB = 5;
+﻿int numberA = new Random().Next(1, 10);// 1, 2, 3, ... 9
+Console.WriteLine (numberA);
+int numberB = new Random().Next(10, 20);// 10, 11, 12, ... 19
+Console.WriteLine (numberB);
 int resalt = numberA+numberB;
 Console.WriteLine (resalt);
 
