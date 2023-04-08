dd70a171c89b8379da54ac3c3b3b8ff17612ef36
juliagrenke <129645624+juliagrenke@users.noreply.github.com>
2023-04-08 22:30:28 +0500

    ДЗ. Задача 8. Вывод всех четных от 1 до N

---
+++ SeminarEx008_AllEvenNum/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine ("Введите число: ");
+int number = int.Parse (Console.ReadLine()!);
+int count = 2;
+//int remainder = number % 2;
+Console.WriteLine ($"Все четные до {number}:");
+if (number % 2 == 0)
+    {
+        while (count <= number)
+        {
+        Console.WriteLine (count);
+        count = count + 2;
+        }  
+    } 
+else
+{
+    number = number - 1;
+    //Console.WriteLine ("не содержит четных");   
+}
+ while (count <= number)
+        {
+        Console.WriteLine (count);
+        count = count + 2;
+        }  
+        
+
