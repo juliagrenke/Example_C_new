e2206939d7f7b94d644c1f35947a8ef41f80bcd7
juliagrenke <129645624+juliagrenke@users.noreply.github.com>
2023-04-08 20:57:06 +0500

    ДЗ. Задача 6. Определение четности введенного числа

---
+++ SeminarEx006_EvenNum/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным 
+//(делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.WriteLine ("Введите число: ");
+int number;
+number = int.Parse (Console.ReadLine()!);
+int remainder = number % 2;
+if (remainder > 0) 
+{
+    Console.WriteLine ("Введенное число нечетное");
+}
+else
+{
+    Console.WriteLine ("Введенное число четное");
+}
