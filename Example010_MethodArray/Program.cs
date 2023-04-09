41a8562393cdab8036785a75b71931c9ec6f0159
juliagrenke <129645624+juliagrenke@users.noreply.github.com>
2023-04-09 20:45:30 +0500

    Example010_MethodArray add

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int [] array = { 1, 32, 15, 76, 32, 91, 13, 32, 72 };
+int n = array.Length; // возвращает количество элементов массива
+int finde = 32;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == finde)
+    {
+        Console.WriteLine(index);
+        break; // прерывает цикл при первом выполнении условия
+    }
+    index++; //index = index+1;
+}
