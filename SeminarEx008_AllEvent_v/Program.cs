46144a0cec89e368132c2f6e43c051bcff745361
juliagrenke <129645624+juliagrenke@users.noreply.github.com>
2023-04-12 11:43:25 +0500

    SeminarEx008_AllEvent_v add вторая версия

---
+++ SeminarEx008_AllEvent_v/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine ("Введите число: ");
+int number = int.Parse (Console.ReadLine());
+int index = 2;
+Console.WriteLine ($"Все четные до {number}:");
+
+while (index <= number)
+{
+Console.WriteLine(index);
+index += 2;
+}
