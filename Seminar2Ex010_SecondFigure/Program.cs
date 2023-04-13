09d8b652e0fd493fb38c6e1ae79c690c98f388b0
juliagrenke <129645624+juliagrenke@users.noreply.github.com>
2023-04-13 11:00:34 +0500

    ДЗ Семинар2. Задача 10. Вывод второй цифры трехзначного числа

---
+++ Seminar2Ex010_SecondFigure/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает 
+вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+int SecondDigit(int number)
+{
+    int result1 = number % 100;
+    int result2 = number % 10;
+    number = ((result1 - result2) / 10);
+    return number;
+}
+
+Console.WriteLine("Введите любое трехзначное число:");
+int number = int.Parse(Console.ReadLine()!);
+Console.WriteLine(SecondDigit(number));
+
+
