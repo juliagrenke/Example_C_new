6c1310e366fa6ec055d4e120265dcec122461b93
juliagrenke <129645624+juliagrenke@users.noreply.github.com>
2023-04-16 21:38:35 +0500

    Seminar3Ex22_TabOfSquare add Таблица квадратов чисел до N

---
+++ Seminar3Ex22_TabOfSquare/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 22. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел
+от 1 до N*/
+
+Console.WriteLine("Введите число N> ");
+int N = Convert.ToInt32(Console.ReadLine()!);
+int[] myarray = new int[N+1];
+int i = 1;
+while (i <= N)
+{
+    myarray[i] = i;
+    Console.Write(Square(i));
+    Console.Write("  ");
+    i++;
+}
+
+
+int Square(int x)
+{
+    int result = x * x;
+    return result;
+}
+
+
