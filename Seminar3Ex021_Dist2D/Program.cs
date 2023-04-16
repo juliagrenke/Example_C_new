81961f0eff129cdac2d305adc514608b0a1b926e
juliagrenke <129645624+juliagrenke@users.noreply.github.com>
2023-04-16 20:04:47 +0500

    Семинар 3. Расстояние между двумя точками 2D

---
+++ Seminar3Ex021_Dist2D/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задача 21. Напишите программу, которая принимает на вход координаты двух точек и 
+находит расстояние между ними в 2D пространстве.
+A (3,6); B (2,1) -> 5,09
+A (7,-5); B (1,-1) -> 7,21*/
+
+double Dist2D(double xa, double ya, double xb, double yb)
+{
+    double dist = Math.Pow(xa - xb, 2) + Math.Pow(ya - yb, 2);
+    dist = Math.Sqrt(dist);
+    dist = Math.Round(dist, 2);
+    return dist;
+}
+
+Console.Write("Введите Xa > ");
+double Xa = Convert.ToInt32(Console.ReadLine()!);
+Console.Write("Введите Ya > ");
+double Ya = Convert.ToInt32(Console.ReadLine()!);
+Console.Write("Введите Xb > ");
+double Xb = Convert.ToInt32(Console.ReadLine()!);
+Console.Write("Введите Yb > ");
+double Yb = Convert.ToInt32(Console.ReadLine()!);
+Console.WriteLine("Расстояние между точками = " + Dist2D(Xa, Ya, Xb, Yb));
