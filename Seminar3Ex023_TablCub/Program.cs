537664f4e0cc1321002c9c885f0706a9b41989d9
juliagrenke <129645624+juliagrenke@users.noreply.github.com>
2023-04-16 21:45:47 +0500

    ДЗ Семинар 3. Задача 23. Таблица кубов от 1 до N

---
+++ Seminar3Ex023_TablCub/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125*/
+
+Console.WriteLine("Введите число N> ");
+int N = Convert.ToInt32(Console.ReadLine()!);
+int[] myarray = new int[N + 1];
+int i = 1;
+while (i <= N)
+{
+    myarray[i] = i;
+    Console.Write(Cub(i));
+    Console.Write("  ");
+    i++;
+}
+
+
+int Cub(int x)
+{
+    int result = x * x * x;
+    return result;
+}
