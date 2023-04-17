ab8e3e0ffc93ad18e14778f2a6ced5becd968964
juliagrenke <129645624+juliagrenke@users.noreply.github.com>
2023-04-17 22:21:26 +0500

    ДЗ Семинар 3. Задача 19. Является ли введенное число палиндромом

---
+++ Seminar3Ex19_Palindrom/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Задача 19
+Напишите программу, которая принимает на вход (пятизначное) ЛЮБОЕ число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да*/
+
+int ReversNumber(int number)
+{
+    int result = 0;
+    while (number > 0)
+    {
+        result *= 10;           //Console.WriteLine($"result = {result}");
+        result += number % 10;  //Console.WriteLine($"result = {result}");
+        number /= 10;           //Console.WriteLine($"number = {number}");
+    }
+    return result;
+}
+
+Console.WriteLine("Введите любое число >");
+int number = int.Parse(Console.ReadLine()!);
+int revnumber = ReversNumber(number);
+if (revnumber == number) Console.WriteLine("Заданное число - палиндром");
+else Console.WriteLine("Заданное число не является палиндромом");
