482afa1d69c4e6dd160a054d5c56bc573abf0ad0
juliagrenke <129645624+juliagrenke@users.noreply.github.com>
2023-04-19 19:41:12 +0500

    Лекция 3. Упорядочивание данных внутри массива2

---
+++ Example013_Array/Program.cs
@@ -0,0 +1,33 @@
+﻿// Упорядочивание данных внутри массива
+
+int[] arr = { 8, 5, 1, 3, 9, 6, 1, 2, 7 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+void SelectionSoft(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        {
+            for (int j = i + 1; j < array.Length; j++)
+            {
+                if (array[j] < array[minPosition]) minPosition = j;
+            }
+            int temporary = array[i];
+            array[i] = array[minPosition];
+            array[minPosition] = temporary;
+        }
+    }
+}
+PrintArray(arr);
+Console.WriteLine();
+SelectionSoft(arr);
+PrintArray(arr);
