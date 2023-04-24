3a9d601262ddbba3fd269e39c6986732bff2d8ed
juliagrenke <129645624+juliagrenke@users.noreply.github.com>
2023-04-24 19:52:12 +0500

    семинар 4. дз. задача 29. заполнение массива и вывод на экран

---
+++ Seminar4Ex029_MassEghch/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]*/
+
+int[] myArray = new int[8];
+Random rnd = new Random();
+Console.Write("[");
+for (int i = 0; i < myArray.Length; i++)
+{
+    myArray[i] = rnd.Next(0, 100);
+    Console.Write(i < myArray.Length - 1 ? (myArray[i] + ", ") : (myArray[i]));//конструкция для печати
+    // Console.WriteLine($"{i}=" + $" {myArray[i]}");
+    //Console.Write($" {myArray[i]}");
+
+}
+Console.Write("]");
+
+
+
+
+
+
+
+
+
+
