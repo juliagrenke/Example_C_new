a9371c31a09cad5ab9ac1c798a397288b23534b6
juliagrenke <129645624+juliagrenke@users.noreply.github.com>
2023-04-24 13:35:14 +0500

    семинар 4. дз. задача 27. сумма цифр в числе

---
+++ Seminar4Ex027_SumOfFigure/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+int SumFigure(int a)
+{
+    int result = 0;
+    while (a > 0)
+    {
+        result += a % 10;
+        a /= 10;
+    }
+    return result;
+}
+
+
+Console.Write("Введите любое число > ");
+int n = Convert.ToInt32(Console.ReadLine()!);
+Console.Write(SumFigure(n));
