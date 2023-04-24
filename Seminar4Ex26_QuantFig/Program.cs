42c48b341dc87eb9a458c96290a616cf5edff600
juliagrenke <129645624+juliagrenke@users.noreply.github.com>
2023-04-24 13:33:30 +0500

    задача 26. количество цифр в числе

---
+++ Seminar4Ex26_QuantFig/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5*/
+
+int QuantFigure(string n)
+{
+    int lenght = n.Length;
+    return lenght++;
+
+}
+
+Console.Write("Введите число > ");
+int n = Convert.ToInt32(Console.ReadLine()!);
+int n1 = Math.Abs (n);
+string n2 = Convert.ToString(n1);
+Console.WriteLine(QuantFigure(n2));
