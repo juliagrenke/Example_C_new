b7e1337e58b044f4026ac3f7acfb112d8d56646d
juliagrenke <129645624+juliagrenke@users.noreply.github.com>
2023-04-24 13:37:27 +0500

    семинар 4. задача 30. массив, заполненный в случайном порядке 0 и 1

---
+++ Seminar4Ex030_RandomArray/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Задача 30: Напишите программу, которая выводит массив из 8 элементов, 
+заполненный нулями и единицами в случайном порядке.
+[1,0,1,1,0,1,0,0]*/
+
+int[] myArray = new int[8];
+Random rnd = new Random();
+Console.Write("[ ");
+for (int i = 0; i < myArray.Length; i++)
+{
+    myArray[i] = rnd.Next(0, 2);
+    Console.Write(i < myArray.Length - 1 ? (myArray[i] + ", ") : (myArray[i]));//конструкция для печати
+    // Console.WriteLine($"{i}=" + $" {myArray[i]}");
+    //Console.Write($" {myArray[i]}");
+
+}
+Console.Write(" ]");
